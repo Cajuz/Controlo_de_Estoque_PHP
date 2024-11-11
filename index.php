de9deeec74d69c689f36c4ebc90fbdab91d26b94
Cajuz <alvaroalvi57@gmail.com>
2024-11-11 20:13:04 -0300

    semantica html

--- index.php
+++ index.php
@@ -6,6 +6,27 @@
 	<title>Página Inicial</title>
 </head>
 <body>
+<header>
+	<img src="" alt="logo">
+	<nav>
+		<ul>
+			<li><a href="$">HOME</li>
+
+			<li><a href="$">Produtores</li>
+
+			<li><a href="$">Fornecedores</li>
+
+			<li><a href="$">Categorias</li>
+	</nav>
+</header>
+
+<main>
+	<h2>Conteúdo</h2>
+</main>
+
+<footer>
+	<h2>Rodapé</h2>
+</footer>
 
 </body>
 </html>
