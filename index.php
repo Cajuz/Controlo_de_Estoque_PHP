f3b664ae1c70cc70360796d59937d4b812b36754
Cajuz <alvaroalvi57@gmail.com>
2024-11-18 20:01:00 -0300

    projeto controle de estoque em php p1

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Página Inicial</title>
+	<link rel="stylesheet" href="Zcss\style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 <body>
 <header>
@@ -22,11 +24,38 @@
 
 <main>
 	<h2>Conteúdo</h2>
+	<div class="container">
+ 	 <!--<div class="row row-cols-4">
+		<div class="col">Dashboard</div>
+		<div class="col">Dashboard</div>
+		<div class="col">Dashboard</div>
+		<div class="col">Dashboard</div>
+  	</div>-->
+
+	<div class="row">
+		<div class="col">Column1</div>
+		<div class="col">Column2</div>
+		<div class="col">Column3</div>
+		<div class="col">Column4</div>
+		<div class="col">Column5</div>
+		<div class="col">Column6</div>
+		<div class="col">Column7</div>
+		<div class="col">Column8</div>
+		<div class="col">Column9</div>
+		<div class="col">Column10</div>
+		<div class="col">Column11</div>
+		<div class="col">Column12</div>
+	</div>
+</div>
 </main>
 
 <footer>
 	<h2>Rodapé</h2>
 </footer>
 
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
 </html>
